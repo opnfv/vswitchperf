bbda085f0f0a44c52b57784d89cd9b4cfa4087e9
aihuia <aihua.li@huawei.com>
2015-02-08 16:26:44 -0800

    Change-Id: I13bcce3104377cac84a736cfd9a3d9df208e91f0 added src directory to - provide mechanism to pull upstream package - single command "make" to build packages. JIRA: VSPERF-25 Signed-off-by: aihuia <aihua.li@huawei.com>

---
+++ src/package-list.mk
@@ -0,0 +1,12 @@
+# Upstream Package List
+#
+# Everything here is defined as its suggested default
+# value, it can always be overriden when invoking Make
+
+# dpdk section
+DPDK_URL ?= git://dpdk.org/dpdk
+DPDK_TAG ?= v1.6.0r0
+
+# OVS section
+OVS_URL ?= https://github.com/openvswitch/ovs
+OVS_TAG ?= v2.3.1
