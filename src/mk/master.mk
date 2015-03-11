b4b06d27b062a65ef98753d7f1d9d340fa773329
Aihua Li <aihua.li@huawei.com>
2015-03-11 14:12:41 -0700

    added mechanism to pull upstream packages and provided single top-level make
    Change-Id: I13bcce3104377cac84a736cfd9a3d9df208e91f0
    JIRA: VSPERF-25
    Signed-off-by: Aihua Li <aihua.li@huawei.com>
    Reviewed-by: Maryam Tahhan <maryam.tahhan@intel.com>
    Reviewed-by: Gene Snider <eugene.snider@huawei.com>
    Reviewed-by: Stephen Finucane <stephen.finucane@intel.com>

---
+++ src/mk/master.mk
@@ -0,0 +1,40 @@
+# Master makefile definitions for project opnfv vswitchperf
+#
+# Copyright (C) 2015 OPNFV
+#
+# Copying and distribution of this file, with or without modification,
+# are permitted in any medium without royalty provided the copyright
+# notice and this notice are preserved.  This file is offered as-is,
+# without warranty of any kind.
+#
+# Contributors:
+#   Aihua Li, Huawei Technologies.
+
+# user has the full control to decide where to put the build by
+# specifying INSTALL_DIR from the make input
+
+# try to read it in from environment
+INSTALL_DIR ?= $(shell echo $$INSTALL_DIR)
+
+# if it is still not set, then set it to default
+ifeq ($(INSTALL_DIR),)
+INSTALL_DIR = /opt/opnfv
+endif
+
+# for debugging Makefile
+# Make V as a synonum for VERBOSE
+ifdef V
+VERBOSE = $(V)
+endif
+
+VERBOSE ?= 0
+
+ifeq ($(VERBOSE),0)
+    AT = @
+else
+    BASH_X = -x
+endif
+
+.DEFAULT_GOAL := all
+
+.PHONY: all clean cleanse clobber
