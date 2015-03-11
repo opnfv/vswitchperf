3d6b0f7086ee958daadbb2491edd06402d86f13c
Aihua Li <aihua.li@huawei.com>
2015-03-11 14:12:41 -0700

    added mechanism to pull upstream packages and provided single top-level make
    Change-Id: I13bcce3104377cac84a736cfd9a3d9df208e91f0
    JIRA: VSPERF-25
    Signed-off-by: Aihua Li <aihua.li@huawei.com>
    Reviewed-by: Maryam Tahhan <maryam.tahhan@intel.com>
    Reviewed-by: Gene Snider <eugene.snider@huawei.com>
    Reviewed-by: Stephen Finucane <stephen.finucane@intel.com>

---
+++ src/mk/make-subsys.mk
@@ -0,0 +1,20 @@
+# Master makefile definitions for project opnfv vswitchperf
+#
+# Copyright (C) 2015 OPNFV
+#
+# Copying and distribution of this file, with or without modification,
+# are permitted in any medium without royalty provided the copyright
+# notice and this notice are preserved.  This file is offered as-is,
+# without warranty of any kind.
+#
+# Contributors:
+#   Aihua Li, Huawei Technologies.
+
+.PHONY: $(SUBDIRS)
+
+all clean cleanse clobber install uninstall: $(SUBDIRS)
+	$(AT)echo "finished making $@"
+
+$(SUBDIRS):
+	$(AT)echo "Enter directory $@"
+	$(AT)$(MAKE) -C $@ $(MAKECMDGOALS)
