374cc3686d4c7275cbe49690eae6f87035e8e483
Billy O'Mahony <billy.o.mahony@intel.com>
2015-07-07 14:26:14 +0100

    testcase: Add frame modification config
    * Added a new standard set of flow table that defaults to routing phy1 to phy2
      and vice-versa and allows frame modification and tuple matches to be added
      independently of routing.
    * Add a FrameMod testcase attribute - implement this for value 'vlan'
    * With FrameMod == 'vlan' the reported tx_fps and rx_fps figures do not match
      even for 0% packet loss - this is a pre-existing bug and a fix will be done
      as a separate patch. The rx_fps figure is the reliable figure.
    
    JIRA: VSPERF-27
    
    Change-Id: Idd137b7a101305e7aebb6fabdfb6a7a4dcf8661b
    Signed-off-by: Billy O'Mahony<billy.o.mahony@intel.com>
    Reviewed-by: Al Morton <acmorton@att.com>
    Reviewed-by: Eugene Snider <Eugene.Snider@huawei.com>
    Reviewed-by: Maryam Tahhan <maryam.tahhan@intel.com>
    Reviewed-by: Gurpreet Singh <gurpreet.singh@spirent.com>
    Reviewed-by: Tv Rao <tv.rao@freescale.com>

--- src/package-list.mk
+++ src/package-list.mk
@@ -4,10 +4,11 @@
 # value, it can always be overriden when invoking Make
 
 # dpdk section
-# DPDK_URL ?= git://dpdk.org/dpdk
 DPDK_URL ?= http://dpdk.org/git/dpdk
-DPDK_TAG ?= v2.0.0
+#..c5fc has fix for array index oob in eal memory
+DPDK_TAG ?= d307f7957c9da6dee264ab7c9b349871c5a4c5fc
 
 # OVS section
 OVS_URL ?= https://github.com/openvswitch/ovs
+#OVS_TAG ?= da79ce2b71dd879e7f20fdddc715568f6a74185a 
 OVS_TAG ?= master 
