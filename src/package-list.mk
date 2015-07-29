fcde83b45471112966369259204e32a6da375f57
Dino Simeon Madarang <dino.simeonx.madarang@intel.com>
2015-07-29 11:00:29 +0100

    src: Add QEMU makefile
    QEMU v2.2.0+ is recommended for vhost-user. We are adding QEMU 2.2.1
    to the makefile because the EPEL repo for CentOS7 only supports QEMU 2.0.0.
    
    JIRA: VSPERF-56
    
    Change-Id: I1c65ad6030888802586659d044f069f7fe57c0ea
    Signed-off-by: Timo Puha <timox.puha@intel.com>
    Signed-off-by: Madarang, Dino Simeon <dino.simeonx.madarang@intel.com>
    Reviewed-by: Billy O Mahony <billy.o.mahony@intel.com>
    Reviewed-by: Maryam Tahhan <maryam.tahhan@intel.com>
    Reviewed-by: Eugene Snider <Eugene.Snider@huawei.com>
    Reviewed-by: Gurpreet Singh <gurpreet.singh@spirent.com>
    Reviewed-by: Tv Rao <tv.rao@freescale.com>

--- src/package-list.mk
+++ src/package-list.mk
@@ -11,3 +11,7 @@ DPDK_TAG ?= d307f7957c9da6dee264ab7c9b349871c5a4c5fc
 # OVS section
 OVS_URL ?= https://github.com/openvswitch/ovs
 OVS_TAG ?= 943f394ea332837d8e5285986c5182e9746c6c62
+
+# QEMU section
+QEMU_URL ?= https://github.com/qemu/qemu.git
+QEMU_TAG ?= v2.2.1
