2d55f23c6b5bcb65577eb7fc1978ad74d62012c5
Abdul Halim <abdul.halim@intel.com>
2015-10-27 16:51:04 +0000

    Adding system build scripts for Fedora 21 and Fedora 22
    Scripts are added for Fedora 21 and 22 to install system requirements
    and python packages on these systems. Also modified root
    build_base_machine.sh so that it can handle directory structures for
    version specific scripts if available.
    
    JIRA: VSPERF-93
    
    Change-Id: If6a311bc7bb1116979faabff5e9097e39f2f5495
    Signed-off-by: Abdul Halim <abdul.halim@intel.com>
    Reviewed-by: Billy O'Mahony<billy.o.mahony@intel.com>
    Reviewed-by: Maryam Tahhan <maryam.tahhan@intel.com>
    Reviewed-by: Martin Klozik <martinx.klozik@intel.com>
    Reviewed-by: Al Morton <acmorton@att.com>
    Reviewed-by: Eugene Snider <Eugene.Snider@huawei.com>

---
+++ systems/Fedora/22/build_base_machine.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+#
+# Build a base machine for Fedora 22
+#
+# Copyright 2015 OPNFV, Intel Corporation.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Contributors:
+#   Aihua Li, Huawei Technologies.
+#   Martin Klozik, Intel Corporation.
+#   Abdul Halim, Intel Corporation.
+
+dnf -y install $(echo "
+# Make and Compilers
+make
+automake
+gcc
+gcc-c++
+libxml2
+glibc.i686
+kernel-devel
+fuse-libs
+fuse
+fuse-devel
+
+# tools
+curl
+autoconf
+libtool
+libpcap-devel
+libnet
+vim
+wget
+git
+pciutils
+cifs-utils
+socat
+
+# install python packages
+python3
+python3-virtualenv
+python3-setuptools
+python3-pip
+python3-dbus
+python3-devel
+python3-tkinter
+systemd-python3
+python3-libs
+libreport-python3
+abrt-python3
+abrt-addon-python3
+
+# install git-review tool
+git-review
+" | grep -v ^#)
