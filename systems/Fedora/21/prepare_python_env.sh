e941321e53c648c7c05063780c7fead6fbcb9459
Abdul Halim <abdul.halim@intel.com>
2015-10-27 16:51:04 +0000

    Adding system build scripts for Fedora 21 and Fedora 22
    Scripts are added for Fedora 21 and 22 to install system requirements
    and python packages on these systems. Also modified root
    build_base_machine.sh so that it can handle directory structures for
    version specific scripts if available.
    
    JIRA: VSPERF-93
    
    Change-Id: If6a311bc7bb1116979faabff5e9097e39f2f5495
    Signed-off-by: Abdul Halim <abdul.halim@intel.com>
    Reviewed-by: Billy O'Mahony<billy.o.mahony@intel.com>
    Reviewed-by: Maryam Tahhan <maryam.tahhan@intel.com>
    Reviewed-by: Martin Klozik <martinx.klozik@intel.com>
    Reviewed-by: Al Morton <acmorton@att.com>
    Reviewed-by: Eugene Snider <Eugene.Snider@huawei.com>
    Reviewed-by: Maryam Tahhan <maryam.tahhan@intel.com>
    Reviewed-by: Gurpreet Singh <gurpreet.singh@spirent.com>
    Reviewed-by: Tv Rao <tv.rao@freescale.com>

---
+++ systems/Fedora/21/prepare_python_env.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#
+# Prepare Python environment for vsperf execution on Fedora systems
+#
+# Copyright 2015 OPNFV, Intel Corporation.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+if [ -d "$VSPERFENV_DIR" ] ; then
+    echo "Directory $VSPERFENV_DIR already exists. Skipping python virtualenv creation."
+    exit
+fi
+
+# enable virtual environment in a subshell, so QEMU build can use python 2.7
+
+(virtualenv-3.4 "$VSPERFENV_DIR"
+source "$VSPERFENV_DIR"/bin/activate
+pip install -r ../requirements.txt
+pip install pylint)
