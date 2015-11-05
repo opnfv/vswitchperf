dfdcd6caf429dd101b0a0c2e81a65000b79d9604
Martin Klozik <martinx.klozik@intel.com>
2015-11-05 10:08:45 +0000

    Enable OVS master and DPDK 2.1
    VSPERF used old version of OVS, which has supported DPDK 2.0.
    With switch to DPDK 2.1, VSPERF can use recent OVS code again.
    
    JIRA: VSPERF-124
    Change-Id: I81f6b8887d795df08a2eb8959606f2a9a277f9e6
    Signed-off-by: Martin Klozik <martinx.klozik@intel.com>
    Reviewed-by: Maryam Tahhan <maryam.tahhan@intel.com>
    Reviewed-by: Brian Castelli <brian.castelli@spirent.com>
    Reviewed-by: Gene Snider <eugene.snider@huawei.com>
    Reviewed-by: Al Morton <acmorton@att.com>
    Reviewed-by: Tv Rao <tv.rao@freescale.com>

--- src/package-list.mk
+++ src/package-list.mk
@@ -6,11 +6,11 @@
 # dpdk section
 # DPDK_URL ?= git://dpdk.org/dpdk
 DPDK_URL ?= http://dpdk.org/git/dpdk
-DPDK_TAG ?= v2.0.0
+DPDK_TAG ?= v2.1.0
 
 # OVS section
 OVS_URL ?= https://github.com/openvswitch/ovs
-OVS_TAG ?= ad2e649834be20dd01b1632799fe778106a96a2d
+OVS_TAG ?= master
 
 # QEMU section
 QEMU_URL ?= https://github.com/qemu/qemu.git
