90db92ea96dfd19d76a368193af0fb8d0678db8c
sridharkn <sridhar@linux.com>
2016-02-10 10:42:56 +0530

    pkt_gen: Adding Support of testcenter traffic generation using STC REST API.
    Stcrestclient package based script is added. Support for RFC22544
    backtoback, latency, frameloss are also added, along with the throughput.
    
    Comments Addressed:
    1.  Parameter name consistency. Ex: use duration (refer dummy.py).
    2.  Moved 'only the necessary contents of README to
        configguide/trafficgen.rst.
    3.  Removed README file
    4.  Commit message format (pkt_gen: and linewrap)
    5.  Merged 4 scripts (throughput/backtoback/latency/frameloss)
        that were rest-based into one single script.
        Name: testcenter-rfc2544-test.py
    6.  Changed year to 2016
    7.  Moved imports to the top (testcenter-rfc2544-rest.py)
    8.  Changed prints to Log (testcenter-rfc2544-rest.py)
    9.  Reporting the frame loss percentage.
    10. Calls to frameloss and latency are not added as the 'caller'
        class TrafficControllerRFC2544 is only calling backtoback and
        throughput.
    11. Changed Exception, e to Exception as e for python 3
        compatibility
    
    JIRA: VSPERF-237
    
    Change-Id: I05d2b24fca1da4ba67bc4d79206c5d755ebf8f20
    Signed-off-by: sridharkn <sridhar@linux.com>

--- tools/pkt_gen/testcenter/testcenter.py
+++ tools/pkt_gen/testcenter/testcenter.py
@@ -1,4 +1,4 @@
-# Copyright 2015 Spirent Communications.
+# Copyright 2016 Spirent Communications.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -26,6 +26,7 @@ from core.results.results_constants import ResultsConstants
 import subprocess
 import os
 import csv
+import logging
 from conf import settings
 
 
@@ -33,6 +34,8 @@ class TestCenter(trafficgen.ITrafficGenerator):
     """
     Spirent TestCenter
     """
+    _logger = logging.getLogger(__name__)
+
     def connect(self):
         """
         Do nothing.
@@ -45,14 +48,13 @@ class TestCenter(trafficgen.ITrafficGenerator):
         """
         pass
 
-    def send_burst_traffic(self, traffic=None, numpkts=100, duration=20, framerate=100):
+    def send_burst_traffic(self, traffic=None, numpkts=100, duration=20):
         """
         Do nothing.
         """
         return None
 
-    def send_cont_traffic(self, traffic=None, duration=30, framerate=0,
-                          multistream=False):
+    def send_cont_traffic(self, traffic=None, duration=20, multistream=False):
         """
         Do nothing.
         """
@@ -64,10 +66,15 @@ class TestCenter(trafficgen.ITrafficGenerator):
         Send traffic per RFC2544 throughput test specifications.
         """
         verbose = False
-
+        framesize = settings.getValue("TRAFFICGEN_STC_FRAME_SIZE")
+        if traffic and 'l2' in traffic:
+            if 'framesize' in traffic['l2']:
+                framesize = traffic['l2']['framesize']
         args = [settings.getValue("TRAFFICGEN_STC_PYTHON2_PATH"),
-                os.path.join(settings.getValue("TRAFFICGEN_STC_TESTCENTER_PATH"),
-                             settings.getValue("TRAFFICGEN_STC_RFC2544_TPUT_TEST_FILE_NAME")),
+                os.path.join(
+                    settings.getValue("TRAFFICGEN_STC_TESTCENTER_PATH"),
+                    settings.getValue(
+                        "TRAFFICGEN_STC_RFC2544_TPUT_TEST_FILE_NAME")),
                 "--lab_server_addr",
                 settings.getValue("TRAFFICGEN_STC_LAB_SERVER_ADDR"),
                 "--license_server_addr",
@@ -111,7 +118,7 @@ class TestCenter(trafficgen.ITrafficGenerator):
                 "--resolution_pct",
                 settings.getValue("TRAFFICGEN_STC_RESOLUTION_PCT"),
                 "--frame_size_list",
-                settings.getValue("TRAFFICGEN_STC_FRAME_SIZE"),
+                str(framesize),
                 "--acceptable_frame_loss_pct",
                 settings.getValue("TRAFFICGEN_STC_ACCEPTABLE_FRAME_LOSS_PCT"),
                 "--east_intf_addr",
@@ -122,33 +129,151 @@ class TestCenter(trafficgen.ITrafficGenerator):
                 settings.getValue("TRAFFICGEN_STC_WEST_INTF_ADDR"),
                 "--west_intf_gateway_addr",
                 settings.getValue("TRAFFICGEN_STC_WEST_INTF_GATEWAY_ADDR")]
+
         if settings.getValue("TRAFFICGEN_STC_VERBOSE") is "True":
             args.append("--verbose")
             verbose = True
-            print("Arguments used to call test: %s" % args)
-
-        subprocess.check_call(map(os.path.expanduser, args))
+            self._logger.debug("Arguments used to call test: %s", args)
+        subprocess.check_call(args)
 
         file = os.path.join(settings.getValue("TRAFFICGEN_STC_RESULTS_DIR"),
-                            settings.getValue("TRAFFICGEN_STC_CSV_RESULTS_FILE_PREFIX") + ".csv")
+                            settings.getValue(
+                            "TRAFFICGEN_STC_CSV_RESULTS_FILE_PREFIX") + ".csv")
         if verbose:
-            print("file: %s" % file)
+            self._logger.info("file: %s", file)
 
         result = {}
 
         with open(file, "r") as csvfile:
             csvreader = csv.DictReader(csvfile)
             for row in csvreader:
-                print("Row: %s" % row)
+                self._logger.info("Row: %s", row)
                 result[ResultsConstants.TX_RATE_FPS] = 0.0
                 result[ResultsConstants.THROUGHPUT_RX_FPS] = 0.0
                 result[ResultsConstants.TX_RATE_MBPS] = 0.0
                 result[ResultsConstants.THROUGHPUT_RX_MBPS] = 0.0
-                result[ResultsConstants.TX_RATE_PERCENT] = float(row["OfferedLoad(%)"])
-                result[ResultsConstants.THROUGHPUT_RX_PERCENT] = float(row["Throughput(%)"])
-                result[ResultsConstants.MIN_LATENCY_NS] = float(row["MinimumLatency(us)"]) * 1000
-                result[ResultsConstants.MAX_LATENCY_NS] = float(row["MaximumLatency(us)"]) * 1000
-                result[ResultsConstants.AVG_LATENCY_NS] = float(row["AverageLatency(us)"]) * 1000
+                result[ResultsConstants.TX_RATE_PERCENT] = float(
+                    row["OfferedLoad(%)"])
+                result[ResultsConstants.THROUGHPUT_RX_PERCENT] = float(
+                    row["Throughput(%)"])
+                result[ResultsConstants.MIN_LATENCY_NS] = float(
+                    row["MinimumLatency(us)"]) * 1000
+                result[ResultsConstants.MAX_LATENCY_NS] = float(
+                    row["MaximumLatency(us)"]) * 1000
+                result[ResultsConstants.AVG_LATENCY_NS] = float(
+                    row["AverageLatency(us)"]) * 1000
+                result[ResultsConstants.FRAME_LOSS_PERCENT] = float(
+                    row["PercentLoss"])
+        return result
+
+    def send_rfc2544_back2back(self, traffic=None, trials=1, duration=20,
+                               lossrate=0.0):
+        """
+        Send traffic per RFC2544 BacktoBack test specifications.
+        """
+        verbose = False
+        framesize = settings.getValue("TRAFFICGEN_STC_FRAME_SIZE")
+        if traffic and 'l2' in traffic:
+            if 'framesize' in traffic['l2']:
+                framesize = traffic['l2']['framesize']
+        args = [settings.getValue("TRAFFICGEN_STC_PYTHON2_PATH"),
+                os.path.join(
+                    settings.getValue("TRAFFICGEN_STC_TESTCENTER_PATH"),
+                    settings.getValue(
+                        "TRAFFICGEN_STC_RFC2544_B2B_TEST_FILE_NAME")),
+                "--metric",
+                settings.getValue("TRAFFICGEN_STC_RFC2544_METRIC"),
+                "--lab_server_addr",
+                settings.getValue("TRAFFICGEN_STC_LAB_SERVER_ADDR"),
+                "--license_server_addr",
+                settings.getValue("TRAFFICGEN_STC_LICENSE_SERVER_ADDR"),
+                "--east_chassis_addr",
+                settings.getValue("TRAFFICGEN_STC_EAST_CHASSIS_ADDR"),
+                "--east_slot_num",
+                settings.getValue("TRAFFICGEN_STC_EAST_SLOT_NUM"),
+                "--east_port_num",
+                settings.getValue("TRAFFICGEN_STC_EAST_PORT_NUM"),
+                "--west_chassis_addr",
+                settings.getValue("TRAFFICGEN_STC_WEST_CHASSIS_ADDR"),
+                "--west_slot_num",
+                settings.getValue("TRAFFICGEN_STC_WEST_SLOT_NUM"),
+                "--west_port_num",
+                settings.getValue("TRAFFICGEN_STC_WEST_PORT_NUM"),
+                "--test_session_name",
+                settings.getValue("TRAFFICGEN_STC_TEST_SESSION_NAME"),
+                "--results_dir",
+                settings.getValue("TRAFFICGEN_STC_RESULTS_DIR"),
+                "--csv_results_file_prefix",
+                settings.getValue("TRAFFICGEN_STC_CSV_RESULTS_FILE_PREFIX"),
+                "--num_trials",
+                settings.getValue("TRAFFICGEN_STC_NUMBER_OF_TRIALS"),
+                "--trial_duration_sec",
+                settings.getValue("TRAFFICGEN_STC_TRIAL_DURATION_SEC"),
+                "--traffic_pattern",
+                settings.getValue("TRAFFICGEN_STC_TRAFFIC_PATTERN"),
+                "--search_mode",
+                settings.getValue("TRAFFICGEN_STC_SEARCH_MODE"),
+                "--learning_mode",
+                settings.getValue("TRAFFICGEN_STC_LEARNING_MODE"),
+                "--latency_type",
+                settings.getValue("TRAFFICGEN_STC_LATENCY_TYPE"),
+                "--rate_lower_limit_pct",
+                settings.getValue("TRAFFICGEN_STC_RATE_LOWER_LIMIT_PCT"),
+                "--rate_upper_limit_pct",
+                settings.getValue("TRAFFICGEN_STC_RATE_UPPER_LIMIT_PCT"),
+                "--rate_initial_pct",
+                settings.getValue("TRAFFICGEN_STC_RATE_INITIAL_PCT"),
+                "--rate_step_pct",
+                settings.getValue("TRAFFICGEN_STC_RATE_STEP_PCT"),
+                "--resolution_pct",
+                settings.getValue("TRAFFICGEN_STC_RESOLUTION_PCT"),
+                "--frame_size_list",
+                str(framesize),
+                "--acceptable_frame_loss_pct",
+                settings.getValue("TRAFFICGEN_STC_ACCEPTABLE_FRAME_LOSS_PCT"),
+                "--east_intf_addr",
+                settings.getValue("TRAFFICGEN_STC_EAST_INTF_ADDR"),
+                "--east_intf_gateway_addr",
+                settings.getValue("TRAFFICGEN_STC_EAST_INTF_GATEWAY_ADDR"),
+                "--west_intf_addr",
+                settings.getValue("TRAFFICGEN_STC_WEST_INTF_ADDR"),
+                "--west_intf_gateway_addr",
+                settings.getValue("TRAFFICGEN_STC_WEST_INTF_GATEWAY_ADDR")]
+
+        if settings.getValue("TRAFFICGEN_STC_VERBOSE") is "True":
+            args.append("--verbose")
+            verbose = True
+            self._logger.info("Arguments used to call test: %s", args)
+        subprocess.check_call(args)
+
+        file = os.path.join(settings.getValue("TRAFFICGEN_STC_RESULTS_DIR"),
+                            settings.getValue(
+                            "TRAFFICGEN_STC_CSV_RESULTS_FILE_PREFIX") + ".csv")
+        if verbose:
+            self._logger.info("file: %s", file)
+
+        result = {}
+
+        with open(file, "r") as csvfile:
+            csvreader = csv.DictReader(csvfile)
+            for row in csvreader:
+                self._logger.info("Row: %s", row)
+                result[ResultsConstants.TX_RATE_FPS] = 0.0
+                result[ResultsConstants.THROUGHPUT_RX_FPS] = 0.0
+                result[ResultsConstants.TX_RATE_MBPS] = 0.0
+                result[ResultsConstants.THROUGHPUT_RX_MBPS] = 0.0
+                result[ResultsConstants.TX_RATE_PERCENT] = float(
+                    row["OfferedLoad(%)"])
+                result[ResultsConstants.THROUGHPUT_RX_PERCENT] = float(
+                    row["Throughput(%)"])
+                result[ResultsConstants.MIN_LATENCY_NS] = float(
+                    row["MinimumLatency(us)"]) * 1000
+                result[ResultsConstants.MAX_LATENCY_NS] = float(
+                    row["MaximumLatency(us)"]) * 1000
+                result[ResultsConstants.AVG_LATENCY_NS] = float(
+                    row["AverageLatency(us)"]) * 1000
+                result[ResultsConstants.FRAME_LOSS_PERCENT] = float(
+                    row["PercentLoss"])
         return result
 
 if __name__ == '__main__':
@@ -159,6 +284,6 @@ if __name__ == '__main__':
             'dstip': '90.90.90.90',
         },
     }
-
     with TestCenter() as dev:
         print(dev.send_rfc2544_throughput(traffic=TRAFFIC))
+        print(dev.send_rfc2544_backtoback(traffic=TRAFFIC))
