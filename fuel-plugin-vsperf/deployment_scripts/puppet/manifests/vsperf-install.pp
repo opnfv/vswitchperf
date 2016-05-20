5df5198c8b178debaf0e7190a42fa6cc2851b633
Guo Ruijing <ruijing.guo@intel.com>
2016-05-20 10:35:39 +0800

    Rebase to fuel 9.0 and add vagrant based build and enhance some scripts
    Change-Id: Id3a38d662c330cd388c6509c9d6e90409c1da6e7
    Signed-off-by: Guo Ruijing <ruijing.guo@intel.com>

--- fuel-plugin-vsperf/deployment_scripts/puppet/manifests/vsperf-install.pp
+++ fuel-plugin-vsperf/deployment_scripts/puppet/manifests/vsperf-install.pp
@@ -1,11 +1,7 @@
 $fuel_settings = parseyaml(file('/etc/astute.yaml'))
 $master_ip = $::fuel_settings['master_ip']
 
-exec { "wget vsperf":
-          command => "wget http://$master_ip:8080/plugins/fuel-plugin-vsperf-1.0/repositories/ubuntu/vswitchperf.tgz -O /opt/vswitchperf.tgz",
-                        path   => "/usr/bin:/usr/sbin:/bin:/sbin",
-}
-exec { "untar vsperf":
-          command => "tar xf /opt/vswitchperf.tgz -C /opt",
-                        path   => "/usr/bin:/usr/sbin:/bin:/sbin",
+exec { "install vsperf":
+    command => "mkdir -p /opt/vswitchperf; curl http://$master_ip:8080/plugins/fuel-plugin-vsperf-1.0/repositories/ubuntu/vswitchperf.tgz | tar xzv -C /opt/vswitchperf",
+    path   => "/usr/bin:/usr/sbin:/bin:/sbin";
 }
