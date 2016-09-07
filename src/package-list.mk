db046f736d251370bce894976f4f304a41dc9d60
Martin Klozik <martinx.klozik@intel.com>
2016-09-07 13:26:07 +0100

    ci: Update default DPDK and OVS versions
    VSPERF was updated to use DPDK16.07 and OVS 2.6.90
    by default. This change should improve a performance.
    It will also solve failures of VSPERF verify and merge
    jobs at OPNFV Jenkins, because Newer OVS version is
    compatible with Linux Kernel 4.4.
    
    JIRA: VSPERF-378
    
    Change-Id: I636dd96facd3f6357f215290eb2382c9cc16150e
    Signed-off-by: Martin Klozik <martinx.klozik@intel.com>
    Reviewed-by: Maryam Tahhan <maryam.tahhan@intel.com>
    Reviewed-by: Al Morton <acmorton@att.com>
    Reviewed-by: Christian Trautman <ctrautma@redhat.com>
    Reviewed-by: Bill Michalowski <bmichalo@redhat.com>

--- src/package-list.mk
+++ src/package-list.mk
@@ -6,13 +6,11 @@
 # dpdk section
 # DPDK_URL ?= git://dpdk.org/dpdk
 DPDK_URL ?= http://dpdk.org/git/dpdk
-DPDK_TAG ?= v16.04
+DPDK_TAG ?= v16.07
 
 # OVS section
 OVS_URL ?= https://github.com/openvswitch/ovs
-#The Tag below is for OVS v2.5.0 with backwards compatibility support for Qemu
-#versions < 2.5.
-OVS_TAG ?= 31871ee3839c35e6878debfc7926afa471dbdec6
+OVS_TAG ?= ed26e3ea9995ba632e681d5990af5ee9814f650e
 
 # QEMU section
 QEMU_URL ?= https://github.com/qemu/qemu.git
