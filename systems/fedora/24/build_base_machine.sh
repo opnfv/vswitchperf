244ee3888b2660c7e7e554c84fde18e27418aa1f
goldammx <martinx.goldammer@intel.com>
2017-01-20 09:17:20 -0500

    fedora24_install: Provide installer script for Fedora 24
    Adds installer script for Fedora 24. Kernel version on Fedora 24
    requires DPDK 16.11 and newer. VSPERF user must ensure, that used DPDK
    and OVS version which are compatible with installed kernel.
    
    Adds new folder for current distribution with installation scripts.
    
    JIRA: VSPERF-453
    
    Change-Id: I08846d6e51faa578ea9305173174bfc49b77018a
    Signed-off-by: Martin Goldammer <martinx.goldammer@intel.com>
    Reviewed-by: Martin Klozik <martinx.klozik@intel.com>
    Reviewed-by: Al Morton <acmorton@att.com>
    Reviewed-by: Christian Trautman <ctrautma@redhat.com>
    Reviewed-by: Bill Michalowski <bmichalo@redhat.com>
    Reviewed-by: Antonio Fischetti <antonio.fischetti@intel.com>
    Reviewed-by: Sridhar Rao <sridhar.rao@spirent.com>

---
+++ systems/fedora/24/build_base_machine.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+#
+# Build a base machine for Fedora 24
+#
+# Copyright 2017 OPNFV, Intel Corporation.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+dnf -y install $(echo "
+# Make and Compilers
+make
+automake
+gcc
+gcc-c++
+libxml2
+glibc
+glib2-devel
+kernel-devel
+fuse-libs
+fuse
+fuse-devel
+kernel-modules-extra
+pixman-devel
+openssl-devel
+
+# tools
+curl
+autoconf
+libtool
+libpcap-devel
+libnet
+vim
+wget
+git
+pciutils
+cifs-utils
+socat
+sysstat
+
+# install python packages
+python3
+python3-virtualenv
+python3-setuptools
+python3-pip
+python3-dbus
+python3-devel
+python3-tkinter
+systemd-python3
+python3-libs
+libreport-python3
+abrt-python3
+abrt-addon-python3
+
+# libs
+numactl
+numactl-devel
+
+# install git-review tool
+git-review
+" | grep -v ^#)
+
+# Create hugepage dirs
+mkdir -p /dev/hugepages
