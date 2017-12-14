opnfv/vswitchperf

commit 21b23522861e364daf150cd997c5d60851847ef2

install: Python virtualenv python3 enforcement
It was reported that virtualenv-3.x sometimes uses python2.7 instead of
python3. This update of the python virtual environment creation scripts,
enforces the use of python3 from /usr/bin/python3.
The scripts were tested on fresh installations of each currently
supported Linux OS.

JIRA:VSPERF-551

Change-Id: If909dd9578a3c42b14c1a3e4f9ed7b07a675fb58
Signed-off-by: Richard Elias <richardx.elias@intel.com>
Reviewed-by: Martin Klozik <martinx.klozik@intel.com>
Reviewed-by: Al Morton <acmorton@att.com>
Reviewed-by: Christian Trautman <ctrautma@redhat.com>
Reviewed-by: Sridhar Rao <sridhar.rao@spirent.com>
Reviewed-by: Trevor Cooper <trevor.cooper@intel.com>

systems/rhel/7.2/prepare_python_env.sh
@@ -2,7 +2,7 @@
 #
 # Prepare Python environment for vsperf execution on Red Hat 7.2 systems.
 #
-# Copyright 2016 OPNFV, Intel Corporation, Red Hat Inc.
+# Copyright 2016-2017 OPNFV, Intel Corporation, Red Hat Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -22,7 +22,7 @@ if [ -d "$VSPERFENV_DIR" ] ; then
 fi
 
 scl enable python33 "
-virtualenv "$VSPERFENV_DIR"
+virtualenv "$VSPERFENV_DIR" --python /usr/bin/python3
 source "$VSPERFENV_DIR"/bin/activate
 pip install -r ../requirements.txt
 pip install pylint

systems/rhel/7.3/prepare_python_env.sh
@@ -2,7 +2,7 @@
 #
 # Prepare Python environment for vsperf execution on RHEL 7.3 systems.
 #
-# Copyright 2016 OPNFV, Intel Corporation, Red Hat Inc.
+# Copyright 2016-2017 OPNFV, Intel Corporation, Red Hat Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -22,7 +22,7 @@ if [ -d "$VSPERFENV_DIR" ] ; then
 fi
 
 scl enable python33 "
-virtualenv "$VSPERFENV_DIR"
+virtualenv "$VSPERFENV_DIR" --python /usr/bin/python3
 source "$VSPERFENV_DIR"/bin/activate
 pip install -r ../requirements.txt
 pip install pylint

systems/centos/prepare_python_env.sh
@@ -2,7 +2,7 @@
 #
 # Prepare Python 3 environment for vsperf execution
 #
-# Copyright 2015 OPNFV
+# Copyright 2015-2017 OPNFV
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -22,7 +22,7 @@ if [ -d "$VSPERFENV_DIR" ] ; then
 fi
 
 scl enable python33 "
-virtualenv "$VSPERFENV_DIR"
+virtualenv "$VSPERFENV_DIR" --python /usr/bin/python3
 source "$VSPERFENV_DIR"/bin/activate
 pip install -r ../requirements.txt
 pip install pylint

systems/fedora/24/prepare_python_env.sh
@@ -23,7 +23,7 @@ fi
 
 # enable virtual environment in a subshell, so QEMU build can use python 2.7
 
-(virtualenv-3.5 "$VSPERFENV_DIR"
+(virtualenv-3.5 "$VSPERFENV_DIR" --python /usr/bin/python3
 source "$VSPERFENV_DIR"/bin/activate
 pip install -r ../requirements.txt
 pip install pylint)

systems/fedora/25/prepare_python_env.sh
@@ -23,7 +23,7 @@ fi
 
 # enable virtual environment in a subshell, so QEMU build can use python 2.7
 
-(virtualenv-3.5 "$VSPERFENV_DIR"
+(virtualenv-3.5 "$VSPERFENV_DIR" --python /usr/bin/python3
 source "$VSPERFENV_DIR"/bin/activate
 pip install -r ../requirements.txt
 pip install pylint

systems/fedora/26/prepare_python_env.sh
@@ -23,7 +23,7 @@ fi
 
 # enable virtual environment in a subshell, so QEMU build can use python 2.7
 
-(virtualenv-3.6 "$VSPERFENV_DIR"
+(virtualenv-3.6 "$VSPERFENV_DIR" --python /usr/bin/python3
 source "$VSPERFENV_DIR"/bin/activate
 pip install -r ../requirements.txt
 pip install pylint

systems/opensuse/42.2/prepare_python_env.sh
@@ -21,7 +21,7 @@ if [ -d "$VSPERFENV_DIR" ] ; then
     exit
 fi
 
-virtualenv "$VSPERFENV_DIR"
+virtualenv "$VSPERFENV_DIR" --python /usr/bin/python3
 source "$VSPERFENV_DIR"/bin/activate
 pip install -r ../requirements.txt
 pip install pylint

systems/opensuse/42.3/prepare_python_env.sh
@@ -21,7 +21,7 @@ if [ -d "$VSPERFENV_DIR" ] ; then
     exit
 fi
 
-virtualenv "$VSPERFENV_DIR"
+virtualenv "$VSPERFENV_DIR" --python /usr/bin/python3
 source "$VSPERFENV_DIR"/bin/activate
 pip install -r ../requirements.txt
 pip install pylint

commit 27bd5cb5fa16cb51260672201cf56351505f0a8f

rhel_path_fix: Fix pathing issue introduce by other commit
Fixes pathing issue with virtual environment creation caused by
commit 21b23522861e364daf150cd997c5d60851847ef2

Change-Id: Ia2c0c44da898e40e122571e477301343d047f974
Signed-off-by: Christian Trautman <ctrautma@redhat.com>

systems/rhel/7.2/prepare_python_env.sh
@@ -22,7 +22,7 @@ if [ -d "$VSPERFENV_DIR" ] ; then
 fi
 
 scl enable python33 "
-virtualenv "$VSPERFENV_DIR" --python /usr/bin/python3
+virtualenv "$VSPERFENV_DIR" --python /opt/rh/python33/root/usr/bin/python3
 source "$VSPERFENV_DIR"/bin/activate
 pip install -r ../requirements.txt
 pip install pylint

systems/rhel/7.3/prepare_python_env.sh
@@ -22,7 +22,7 @@ if [ -d "$VSPERFENV_DIR" ] ; then
 fi
 
 scl enable python33 "
-virtualenv "$VSPERFENV_DIR" --python /usr/bin/python3
+virtualenv "$VSPERFENV_DIR" --python /opt/rh/python33/root/usr/bin/python3
 source "$VSPERFENV_DIR"/bin/activate
 pip install -r ../requirements.txt
 pip install pylint