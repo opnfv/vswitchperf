bc10d5802e28346c852a15fdfc52f6a0d092b60f
Jose Lausuch <jalausuch@suse.com>
2018-03-26 18:25:02 +0200

    Fix packages for SLES15
    Change-Id: Id01c4f76c146a0f0630bf1926ece48aabff0bfa1
    Signed-off-by: Jose Lausuch <jalausuch@suse.com>

--- systems/sles/15/build_base_machine.sh
+++ systems/sles/15/build_base_machine.sh
@@ -65,14 +65,15 @@ libpixman-1-0-devel
 libtool
 libpcap-devel
 libnet9
-libncurses5
+libncurses6
 libcurl4
 libcurl-devel
 libxml2
 libfuse2
-libopenssl1_1_0
+libopenssl1_1
 libopenssl-devel
 libpython3_6m1_0
+libzmq5
 
 " | grep -v ^#)
 
